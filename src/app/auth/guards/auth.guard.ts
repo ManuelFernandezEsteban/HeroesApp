d04e7b3e6c2a7cc78375eb0bb325d1301885b5e5
Manuel1977 <Manuel_fe1977@hotmail.com>
2021-10-06 23:15:57 +0200

    aplicacion lista

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,34 +1,53 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, Router } from '@angular/router';
+import { Observable, pipe } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements  CanLoad {
+export class AuthGuard implements  CanLoad,CanActivate {
 
-  constructor(private authService:AuthService){
+  constructor(private authService:AuthService,
+              private route:Router){
 
   }
-
-
- /* canActivate(
+  canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }*/
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean  {
+      return this.authService.verificaAutenticacion().pipe(
+        tap(estaAutenticado=>{
+          if(!estaAutenticado){
+            this.route.navigate(['./auth/login']);
+          }
+        })
+      );
 
+    /*
+      if (this.authService.auth.id){
+        return true;
+      } 
+      console.log('Bloqueado por el AuthGuard-CanActivate');
+      return false;*/
+  }
 
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean  {
 
-
+      return this.authService.verificaAutenticacion().pipe(
+        tap(estaAutenticado=>{
+          if(!estaAutenticado){
+            this.route.navigate(['./auth/login']);
+          }
+        })
+      );
+/*
     if (this.authService.auth.id){
       return true;
     } 
-
-    return false;
+    console.log('Bloqueado por el AuthGuard-CanLoad');
+    return false;*/
   }
 }
